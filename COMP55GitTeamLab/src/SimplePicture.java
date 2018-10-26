79f513b98e5c8a9736d9a436a28ee068f7346537
Shalayna <shalaynadoe@gmail.com>
2018-10-26 10:36:37 -0700

    fixed

--- COMP55GitTeamLab/src/SimplePicture.java
+++ COMP55GitTeamLab/src/SimplePicture.java
@@ -16,11 +16,8 @@ public class SimplePicture extends GraphicsProgram {
 		GImage robot = new GImage("robot.jpg", 200, 100);
 		add(robot);
 
-<<<<<<< HEAD
 		GLabel label = new GLabel("HAHA I AM BEING LAZY - martyr", 200, 400);
-=======
-		GLabel label = new GLabel("HAHA I AM BEING LAZY #hackslash more changes in Glabel", 200, 400);
->>>>>>> branch 'master' of https://github.com/comp55-fall18/git-team-lab-dots.git
+
 		add(label);
 	}
 }
