94c0c280c2e62efd03de4dd78dfd74379eb1eb03
Bette <e_orr@u.pacific.edu>
2018-10-26 10:38:32 -0700

    Nice. (Changed the GLabel)

--- COMP55GitTeamLab/src/SimplePicture.java
+++ COMP55GitTeamLab/src/SimplePicture.java
@@ -15,7 +15,7 @@ public class SimplePicture extends GraphicsProgram {
 	public void run() {
 		GImage robot = new GImage("robot.jpg", 200, 100);
 		add(robot);
-		GLabel label = new GLabel("HAHA I AM BEING LAZY", 200, 400);
+		GLabel label = new GLabel("Nice. Nice. Nice. Nice. Nice.", 200, 400);
 		add(label);
 	}
 }
